4863a37aad8e2d598601b9f2fa5746ca7b89193c
Sri <sri.ramanujam99@gmail.com>
2023-02-06 16:33:37 -0500

    pre-processing

--- raspi/pre_process.py
+++ raspi/pre_process.py
@@ -1,8 +1,10 @@
 import pandas as pd
+import mne
+import numpy as np
 
 print("Opening file...")
 
-TEST_FILENAME = "1675044441-raw.csv"
+TEST_FILENAME = "rawData.csv"
 # ask for file
 """
 import easygui
@@ -12,49 +14,27 @@ if path is None:
     print("Error: No file chosen")
     quit()
 """
-df = pd.read_csv(TEST_FILENAME)
+csv = pd.read_csv(TEST_FILENAME)
+#rawdata = [csv[' ch1'],csv[' ch2']]
+rawdata = [csv[' ch1']/8 - csv[' ch2']/8]
+info = mne.create_info(1,240, 'eeg')
+data = mne.io.RawArray(rawdata, info)
 
-df.set_index("time", inplace=True)
-
-# PGA gain
-df /= 8
-
-print("File opened and read")
-
-import matplotlib.pyplot as plt
-f, a = plt.subplots(nrows = 4, ncols = 1)
-
-df.plot(ax=a[0], use_index=True, y='ch1')
-df.plot(ax=a[1], use_index=True, y='ch2')
+notched = data.copy().notch_filter(freqs=[60], picks=mne.pick_types(data.info, eeg=True), method="spectrum_fit", filter_length="10s")
+#data.plot(scalings='auto')
+#notched.plot(scalings='auto')
 
 #SOURCE: Makoto's preprocessing pipeline, sccn.ucsd.edu
-
-samples = len(df['ch1'])
-filt_df = df.copy()
-fs = samples/(filt_df.index[-1])
-
-print("SAMPLE RATE", fs)
-
-from scipy import signal
 import numpy as np
+#spectrum.plot_topomap(ch_type='eeg', agg_fun=np.median)
+#Step 1: Add a 60hz notch filter
+notched.plot_psd(average=True)
+#Step 2: High Pass at 0.2 Hz
+highpassed = notched.copy().filter(h_freq=None, l_freq=0.2)
+#Step 3: Low Pass 20hz
+lowpassed = highpassed.copy().filter(l_freq=None, h_freq=20)
+#Step 4: graph
+end = lowpassed.copy()
 
-#Step 1: High-pass filter at 1Hz
-hp = signal.butter(5, 1, 'highpass', output='sos', fs=fs)
-ch1 = signal.sosfilt(hp, filt_df['ch1'])
-ch2 = signal.sosfilt(hp, filt_df['ch2'])
-
-#Step 2: Low-pass at 60hz
-lp = signal.butter(5, 60, 'lowpass', output='sos', fs=fs)
-ch1 = signal.sosfilt(lp, ch1)
-ch2 = signal.sosfilt(lp, ch2)
-
-#END
-filt_df['ch1'] = ch1
-filt_df['ch2'] = ch2
-
-filt_df.plot(ax=a[2], use_index=True, y='ch1')
-filt_df.plot(ax=a[3], use_index=True, y='ch2')
-
-plt.show()
-
-
+end.plot(scalings="auto")
+end.plot_psd()
